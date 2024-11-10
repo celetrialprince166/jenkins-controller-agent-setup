ae7c587a8e3ba845f359518d86cff77d1cf79735
celetrialPrince166 <princeayiku5@gmai.com>
2024-11-10 12:49:44 +0000

    new update

--- terraform/efs/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-module "efs_module" {
-  source = "../modules/efs"
-  vpc_id     = "vpc-03d5d5463539324a9"
-  subnet_ids = ["subnet-084df0b2b09bb132f", "subnet-00ce661761702feac", "subnet-06bbd168f61b29fa3"]
-}
